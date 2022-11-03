1deda09b0e3a7975eb3a3a21248b05b723c148e0
FabioLongoni <longoni.fabio90@gmail.com>
2022-11-03 10:42:32 +0100

    implement post in show.vue

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::resource('posts','Api\PostController')->only(['index','show']);
+Route::resource( 'posts' , 'Api\PostController' )->only([ 'index' , 'show' ])->parameters([ 'posts' => 'slug' ]);
